ef77d68d3499a8486e47ca7e248711dfa28d0072
Alex Sumenko <alexsumenko@Alexs-MacBook-Pro.local>
2020-11-23 15:44:27 +0200

    add splash image styles

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import { Logo } from '../images/Images';
+
 import './Splash.scss';
 
 const Splash = () => {
-  return <h1>Splash Works</h1>;
+  return (
+    <div className='splash'>
+      <Logo height='120rem' width='100%' alt='Splash image' />
+      <div className='splash__text'>
+        <p className='splash__header'>GET READY FOR THE AUTUMN</p>
+        <p className='splash__subheader'>
+          WE HAVE PREPARED EVERYTTHING FOR YOU!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Splash;
